4346cd691f70663a3a9643b009078fafe675afbf
Tom Boutell <tom@punkave.com>
2013-09-27 09:59:32 -0400

    Compatible with the new browser side javascript class pattern

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,43 +1,44 @@
-apos.widgetTypes.rss = {
-  label: 'RSS',
-  editor: function(options) {
-    var self = this;
-        
-    if (!options.messages) {
-      options.messages = {};
+// @class Editor for RSS feed widgets
+
+function AposRssWidgetEditor(options) {
+  var self = this;
+
+  if (!options.messages) {
+    options.messages = {};
+  }
+  if (!options.messages.missing) {
+    options.messages.missing = 'Paste in an RSS feed URL first.';
+  }
+
+  self.type = 'rss';
+  options.template = '.apos-rss-editor';
+
+  self.prePreview = getFeed;
+  self.preSave = getFeed;
+
+  AposWidgetEditor.call(self, options);
+
+  self.afterCreatingEl = function() {
+    self.$feed = self.$el.find('[name="feed"]');
+    self.$feed.val(self.data.feed);
+    self.$limit = self.$el.find('[name="limit"]');
+    self.$limit.val(self.data.limit);
+    setTimeout(function() {
+      self.$feed.focus();
+      self.$feed.setSelection(0, 0);
+    }, 500);
+  };
+
+  function getFeed(callback) {
+    self.exists = !!self.$feed.val();
+    if (self.exists) {
+      self.data.feed = self.$feed.val();
+      self.data.limit = self.$limit.val();
     }
-    if (!options.messages.missing) {
-      options.messages.missing = 'Paste in an RSS feed URL first.';
-    }
+    return callback();
+  }
+}
 
-    self.afterCreatingEl = function() {
-      self.$feed = self.$el.find('[name="feed"]');
-      self.$feed.val(self.data.feed);
-      self.$limit = self.$el.find('[name="limit"]');
-      self.$limit.val(self.data.limit);
-      setTimeout(function() {
-        self.$feed.focus();
-        self.$feed.setSelection(0, 0);
-      }, 500);
-    };
+AposRssWidgetEditor.label = 'RSS Feed';
 
-    self.type = 'rss';
-    options.template = '.apos-rss-editor';
-
-    self.prePreview = getFeed;
-    self.preSave = getFeed;
-
-    function getFeed(callback) {
-      self.exists = !!self.$feed.val();
-      if (self.exists) {
-        self.data.feed = self.$feed.val();
-        self.data.limit = self.$limit.val();
-      }
-      return callback();
-    }
-
-    // Parent class constructor shared by all widget editors
-    apos.widgetEditor.call(self, options);
-  },
-};
-
+apos.addWidgetType('rss');
