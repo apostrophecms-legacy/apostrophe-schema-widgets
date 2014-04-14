80217d343a9b1dd276fa6faf23a41acada4bacb8
Tom Boutell <tom@punkave.com>
2014-04-14 09:08:09 -0400

    * widget manager object === "self" of module, for easier subclassing * Compatible with latest changes

--- index.js
+++ index.js
@@ -32,56 +32,55 @@ function Construct(options, callback) {
   self.pushAsset('script', 'editor', { when: 'user' });
   self.pushAsset('stylesheet', 'content', { when: 'always' });
 
-  apos.itemTypes.rss = {
-    widget: true,
-    label: 'RSS Feed',
-    css: 'rss',
-    icon: 'rss',
-    sanitize: function(item) {
-      if (!item.feed.match(/^https?\:\/\//)) {
-        item.feed = 'http://' + item.feed;
-      }
-      item.limit = parseInt(item.limit, 10);
-    },
-    render: function(data) {
-      return apos.partial('rss', data, __dirname + '/views');
-    },
-
+  self.widget = true;
+  self.label = options.label || 'RSS Feed';
+  self.css = options.css || 'rss';
+  self.icon = options.icon || 'rss';
+  self.sanitize = function(item) {
+    if (!item.feed.match(/^https?\:\/\//)) {
+      item.feed = 'http://' + item.feed;
+    }
+    item.limit = parseInt(item.limit, 10);
+  };
+  self.renderWidget = function(data) {
+    return self.render('rss', data);
+  };
+  self.load = function(req, item, callback) {
     // Asynchronously load the actual RSS feed
     // The properties you add should start with an _ to denote that
     // they shouldn't become data attributes or get stored back to MongoDB
+    item._entries = [];
 
-    load: function(req, item, callback) {
-      item._entries = [];
-
-      var now = new Date();
-      // Take all properties into account, not just the feed, so the cache
-      // doesn't prevent us from seeing a change in the limit property right away
-      var key = JSON.stringify({ feed: item.feed, limit: item.limit });
-      if (cache.hasOwnProperty(key) && ((cache[key].when + lifetime) > now.getTime())) {
-        item._entries = cache[key].data;
-        return callback();
-      }
-
-      feedparser.parseUrl(item.feed).on('complete', function(meta, articles) {
-        articles = articles.slice(0, item.limit);
-
-        // map is native in node
-        item._entries = articles.map(function(article) {
-          return {
-            title: article.title,
-            body: article.description,
-            date: article.pubDate
-          };
-        });
-        // Cache for fast access later
-        cache[key] = { when: now.getTime(), data: item._entries };
-        return callback();
-      }).on('error', function(error) {
-        item._failed = true;
-        return callback();
-      });
+    var now = new Date();
+    // Take all properties into account, not just the feed, so the cache
+    // doesn't prevent us from seeing a change in the limit property right away
+    var key = JSON.stringify({ feed: item.feed, limit: item.limit });
+    if (cache.hasOwnProperty(key) && ((cache[key].when + lifetime) > now.getTime())) {
+      item._entries = cache[key].data;
+      return callback();
     }
+
+    feedparser.parseUrl(item.feed).on('complete', function(meta, articles) {
+      articles = articles.slice(0, item.limit);
+
+      // map is native in node
+      item._entries = articles.map(function(article) {
+        return {
+          title: article.title,
+          body: article.description,
+          date: article.pubDate
+        };
+      });
+      // Cache for fast access later
+      cache[key] = { when: now.getTime(), data: item._entries };
+      return callback();
+    }).on('error', function(error) {
+      item._failed = true;
+      return callback();
+    });
   };
+
+  apos.addWidgetType('rss', self);
+
   return setImmediate(function() { return callback(null); });
 }
